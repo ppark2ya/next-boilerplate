10614d54c9ad8e0cc0a8b5ca300cf4ad09bab260
jtpark <vigabhan1231@gmail.com>
2021-12-18 15:04:15 +0900

    chore: 보일러 플레이트 초기 세팅 완료

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    // alias 추가
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+
+    return config;
+  },
+};
